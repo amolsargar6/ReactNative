bd43b4b6ecc87b3a71cb0871065941e9ef370938
AMOL SARGAR <amolsargar6@gmail.com>
2025-06-06 21:54:56 +0530

    Expense tracker application

---
+++ ExpenseTracker/src/App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import HelloWorld from './screens/HelloWorld'
+
+const App = () => {
+  return (
+    <View>
+      <HelloWorld />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
